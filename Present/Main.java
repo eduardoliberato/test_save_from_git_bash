8ec8431058b8700bdb5d10654c3f520a186ae60a
eduardoliberato <eduardo.d.liberato@gmail.com>
2023-08-13 16:15:03 -0300

    Input some texts

--- Present/Main.java
+++ Present/Main.java
@@ -12,6 +12,11 @@ public class Main {
 
     System.out.println("Hello God, please let me be happy and rich");
 
+    System.out.println("Love: 10/10");
+    System.out.println("Wealth: 10/10");
+    System.out.println("Peace: 10/10");
+    System.out.println("Health: 10/10");
+
 
     }
 
